10e4216752175950958dbfc77effe0ea23da6943
almightyGOSU <gosujinyao@gmail.com>
2015-09-05 13:37:51 +0800

    Trying to add a folder for fb crawler

---
+++ Simple_Facebook_Crawler/src/facebook4jtesting/Facebook4JTester.java
@@ -0,0 +1,13 @@
+package facebook4jtesting;
+
+/** This is an empty class **/
+
+public class Facebook4JTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/** You just got smoked! **/
+	}
+
+}
