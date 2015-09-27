877fc28cd3b083ebf9156900d076a70a41c0a1b2
almightyGOSU <gosujinyao@gmail.com>
2015-09-27 15:06:43 +0800

    Minor edits

--- Simple_Facebook_Crawler/src/facebook4jtesting/SimpleBackendTester.java
+++ Simple_Facebook_Crawler/src/facebook4jtesting/SimpleBackendTester.java
@@ -6,8 +6,10 @@ public class SimpleBackendTester {
 
 	public static void main (String [] args) {
 		
-		// Apple - 3, Banana - 3, Durian - 1, Orange - 1, Watermelon - 1
-		JSONHelper.postJSONContent("www.abc.com/123", "BANANA BANANA APPLE ORANGE BANANA");
-		JSONHelper.postJSONContent("www.abc.com/124", "APPLE APPLE WATERMELON DURIAN");
+		// Apple - 4, Banana - 4, Durian - 1, Orange - 1, Watermelon - 1
+		JSONHelper.postJSONContent(
+				"www.abc.com/123", "BANANA BANANA APPLE ORANGE BANANA. BANANA.");
+		JSONHelper.postJSONContent(
+				"www.abc.com/124", "APPLE APPLE WATERMELON DURIAN. APPLE.");
 	}
 }
